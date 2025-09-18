0dfbc2ebd0d8a876e317b714007af0029b048a6c
NatVenture <natventure@example.com>
2025-09-18 20:57:59 +0300

    Add capacitor config for CI

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jesper.svampkartan',
+  appName: 'Svampkartan',
+  webDir: 'out'
+};
+
+export default config;
